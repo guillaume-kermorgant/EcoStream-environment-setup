1b90af2d9a27059840757b7738f07971c18bbbe8
gkermo <>
2025-04-11 18:24:19 +0200

    Add tofu backend

--- tofu/backend.tf
+++ tofu/backend.tf
@@ -1,4 +1,9 @@
 terraform {
   backend "http" {
+    address = "https://gitlab.com/api/v4/projects/68895536/terraform/state/${var.env_name}"
+    lock_address = "https://gitlab.com/api/v4/projects/68895536/terraform/state/${var.env_name}/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/68895536/terraform/state/${var.env_name}/lock"
+    username = "${var.gitlab_username}"
+    password = "${var.gitlab_token}"
   }
 }
--- tofu/tofu.tfvars
+++ tofu/tofu.tfvars
@@ -1,3 +1,3 @@
-region         = "eu-west-3"
-ami_id         = "ami-0669b163befffbdfc" # Amazon Linux 2 in Paris (check for your region)
-instance_type  = "t2.micro"
+region        = "eu-west-3"
+ami_id        = "ami-0669b163befffbdfc" # Amazon Linux 2 in Paris (check for your region)
+instance_type = "t2.micro"
